e6db8a1cc19cc376d6f23afee6cafaf85565ae91
Felipe Machado <loudenvier@gmail.com>
2024-09-25 17:28:10 -0300

    Refactored demo program to use top-level statements

--- src/ReadLine.Demo/Program.cs
+++ src/ReadLine.Demo/Program.cs
@@ -1,39 +1,28 @@
 ﻿using System;
 
-#pragma warning disable IDE0130 // Namespace does not match folder structure
-namespace ConsoleApplication
-#pragma warning restore IDE0130 // Namespace does not match folder structure
+Console.WriteLine("ReadLine Library Demo");
+Console.WriteLine("---------------------");
+Console.WriteLine();
+
+string[] history = ["ls -a", "dotnet run", "git init"];
+ReadLine.AddHistory(history);
+
+ReadLine.AutoCompletionHandler = new AutoCompletionHandler();
+
+string input = ReadLine.Read("(prompt)> ");
+Console.WriteLine(input);
+
+input = ReadLine.ReadPassword("Enter Password> ");
+Console.WriteLine(input);
+
+class AutoCompletionHandler : IAutoCompleteHandler
 {
-    public class Program
+    public char[] Separators { get; set; } = [' ', '.', '/', '\\', ':'];
+    public string[] GetSuggestions(string text, int index)
     {
-        public static void Main()
-        {
-            Console.WriteLine("ReadLine Library Demo");
-            Console.WriteLine("---------------------");
-            Console.WriteLine();
-
-            string[] history = ["ls -a", "dotnet run", "git init"];
-            ReadLine.AddHistory(history);
-
-            ReadLine.AutoCompletionHandler = new AutoCompletionHandler();
-
-            string input = ReadLine.Read("(prompt)> ");
-            Console.WriteLine(input);
-
-            input = ReadLine.ReadPassword("Enter Password> ");
-            Console.WriteLine(input);
-        }
-    }
-
-    class AutoCompletionHandler : IAutoCompleteHandler
-    {
-        public char[] Separators { get; set; } = [' ', '.', '/', '\\', ':'];
-        public string[] GetSuggestions(string text, int index)
-        {
-            if (text.StartsWith("git "))
-                return ["init", "clone", "pull", "push"];
-            else
-                return null;
-        }
+        if (text.StartsWith("git "))
+            return ["init", "clone", "pull", "push"];
+        else
+            return null;
     }
 }
